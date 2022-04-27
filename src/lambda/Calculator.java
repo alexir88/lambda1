944dc238538520a89e83382a2bc27a77157b41e8
alexir88 <101215694+alexir88@users.noreply.github.com>
2022-04-28 00:09:12 +0300

    Initial commit

---
+++ src/lambda/Calculator.java
@@ -0,0 +1,29 @@
+package lambda;
+
+
+import java.util.function.*;
+
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> divide = (x, y) -> {
+        if (y != 0) System.out.println(x / y);
+        else System.out.println("division by zero");
+        return 0;
+    };
+
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+}
+
